57b24a9fc28be7c69b9da622a232e7eb76d557ea
Andy Diep <86205703+hydr0blade@users.noreply.github.com>
2023-10-23 15:57:46 -0400

    Added all preliminary data metrics to data collection
    Includes: 
    (1) Total number of commands 
    (2) Total number of attackers
    (3) Total number of sessions
    (4) Total number of high, medium, and low risk commands 
    
    (1) and (4) are not measured the same way, this needs to be fixed somehow

--- scripts/data_collection.sh
+++ scripts/data_collection.sh
@@ -3,49 +3,80 @@
 high_risk_count=0
 medium_risk_count=0
 low_risk_count=0
+total_sessions=0
 commands_array=()
+attacker_ip_array=()
+
+high_risk_commands_list=("tar" "unzip" "mv" "rm" "echo" "cp" "chmod" "mkdir" "mount" "wget" "ftp" "curl" "git clone" "lwp-download" "./" "cround" "httpd" "perl" "*.pl" "passwd" "export" "PATH=" "kill" "nano" "pico" "vi" "vim" "sshd" "useradd" "userdel")
+medium_risk_commands_list=("w" "id" "whoami" "last" "ps" "cat/etc/*" "history" "cat .bash_history" "php -v" "uptime" "ifconfig" "uname" "cat /proc/cpuinfo")
+low_risk_commands_list=("cd" "ls" "bash" "exit" "logout" "cat" "shutdown")
 
 # Loop through each honeypot configuration directory
 for DIRECTORY in /home/student/host_logs/*/
 do
+   echo "Directory: $DIRECTORY"
    # Loop through each log file in each directory
    for FILE in "$DIRECTORY"/*
    do
+      echo "File: $FILE"
       # The command after last pipe removes leading whitespace
       commands=$(cat "$FILE" | grep reader | cut -d ':' -f 4 | sed 's/^ *//g')
-      echo "$commands"
-      # Adds each line output from grep to an array
       while IFS= read -r line; do
-         commands_array+=("$line")
+         if [ -n "$line" ]
+         then
+            commands_array+=("$line")
+         fi
       done <<< "$commands"
+      attacker_ips=$(cat "$FILE" | grep "Attacker connected" | cut -d ' ' -f 8 | sort --unique)
+      num_sessions=$(cat "$FILE" | grep -c "Attacker connected")
+      total_sessions=$((total_sessions+num_sessions))
+      while IFS= read -r line; do
+         if [[ ! " ${attacker_ip_array[*]} " =~ ${line} ]]
+         then
+            attacker_ip_array+=("$line")
+         fi
+      done <<< "$attacker_ips"
+      echo "$attacker_ips"
    done
 done
 
 for COMMAND in "${commands_array[@]}"
 do
-   # If the element in the array is not of length 0
-   if [ -n "$COMMAND" ]
-   then
-      # echo $FILE
-      echo "$COMMAND"
-      # Use the rest of the classifications from research paper
-      # Low risk category
-      if [[ "$COMMAND" == *"exit"* ]]
+   #echo "$COMMAND"
+   # Use the classifications from research paper
+
+   # Low risk category
+   for LRC in "${low_risk_commands_list[@]}"
+   do
+      if [[ "$COMMAND" == *"$LRC"* ]]
       then
          low_risk_count=$((low_risk_count+1))
-      # Medium risk category
-      elif [[ "$COMMAND" == *"whoami"* ]]
+      fi
+   done
+   # Medium risk category
+   for MRC in "${medium_risk_commands_list[@]}"
+   do
+      if [[ "$COMMAND" == *"$MRC"* ]]
       then
          medium_risk_count=$((medium_risk_count+1))
-      # High risk category
-      elif [[ "$COMMAND" == *"./"* ]]
+      fi
+   done
+   # High risk category
+   for HRC in "${high_risk_commands_list[@]}"
+   do
+      if [[ "$COMMAND" == *"$HRC"* ]]
       then
          high_risk_count=$((high_risk_count+1))
-      # Do we need an else statement for commands not listed in the paper?
       fi
-   fi
+   done
+   # Do we need an else statement for commands not listed in the paper?
+   # fi
 done
 
+echo $'\nTotal Number of Commands:' ${#commands_array[@]}
+echo "Number of Attackers:" ${#attacker_ip_array[@]}
+echo "Number of Sessions:" $total_sessions
 echo "Number of High Risk Commands:" $high_risk_count
 echo "Number of Medium Risk Commands:" $medium_risk_count
 echo "Number of Low Risk Commands:" $low_risk_count
+
