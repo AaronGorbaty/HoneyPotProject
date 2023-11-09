5a229e3f1814a6e09d7ac35a37817685d3ea5b98
AaronGorbaty <79929603+AaronGorbaty@users.noreply.github.com>
2023-11-09 14:12:24 -0500

    11/09 attacker_detection.sh
    current attacker_detection.sh
    
    #54 still an issue at this point

--- scripts/attacker_detection.sh
+++ scripts/attacker_detection.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 
-log_file="$1"
-container_name=$2
-external_ip=$3
-mitm_port=$4
-recycler_script="/home/student/scripts/recycler.sh"
+LOG_FILE="$1"
+CONTAINER_NAME=$2
+EXTERNAL_IP=$3
+MITM_PORT=$4
+RECYCLER_SCRIPT="/home/student/scripts/recycler.sh"
 
-echo "Sleeping for 5 seconds..."
-sleep 5s
 
-tail -f -n 0 "$log_file" | while read -r line; do
-  # When 'tail -f' detects a new line in the file, it will enter this loop
-  echo triggered
-  #grabbing the attackers IP so that we can block all other IPs
-  attacker_ip=$(grep "Attacker connected:" "$log_file"| sed -n -E 's/.* ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) .*/\1/p' | head -n 1)
-  if tail -f -n 1 "$log_file" | grep -q "Attacker authenticated and is inside container"; then
-    $recycler_script $container_name $external_ip $mitm_port $attacker_ip &
-  fi
-  exit 0
+# Grabbing the attackers IP so that we can block all other IPs
+# ATTACKER_IP=$(grep "Attacker connected:" "$LOG_FILE"| sed -n -E 's/.* ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) .*/\1/p' | head -n 1)
+
+# While loop that does nothing (represented by ':' ) and proceeds with the rest of the script once an attacker is inside the container
+while ! grep -q "Compromising the honeypot" "$LOG_FILE";
+do
+  sleep .5
 done
+
+# Grabbing the attackers IP so that we can block all other IPs
+ATTACKER_IP=$(grep "Threshold" "$LOG_FILE" | sed -n -E 's/.* ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) .*/\1/p' | head -n 1)
+
+# Apply appropriate firewall rules to only allow the attacker currently attacking, once the attacker gets into the container
+# Limit only one session for the root user for the container, prevents two attackers from logging in at the same time
+sudo lxc-attach -n $CONTAINER_NAME -- sudo ufw delete allow 'OpenSSH'
+sudo lxc-attach -n $CONTAINER_NAME -- sudo ufw allow from $ATTACKER_IP to any port 22 proto tcp
+sudo lxc-attach -n $CONTAINER_NAME -- sudo ufw deny 22/tcp
+
+
+# Using Bash built in variable SECONDS, the while loop will run until 5 minutes is up
+# Or if the grep if statement returns true, meaning the attacker exited the container
+SECONDS=0
+# Keep track of number of commands run, in order to track idle time
+NUM_COMMANDS=0
+while [[ $SECONDS -lt 600 ]];
+do
+  # Sleep for 30 seconds, which will be the idle time
+  sleep 30
+  CURR_COUNT=$(grep -E "reader|Noninteractive" "$LOG_FILE" | wc -l)
+  if [[ $CURR_COUNT -ne $NUM_COMMANDS ]]; then
+    # Update count of number of commands run so far
+    NUM_COMMANDS=$CURR_COUNT
+  else
+    # This will exit out of the while loop, if the attacker has been idled for 30 seconds
+    break
+  fi
+done
+
+# Call the recycling script to recycle the container
+$RECYCLER_SCRIPT $CONTAINER_NAME $EXTERNAL_IP $MITM_PORT &
+
+exit 0
