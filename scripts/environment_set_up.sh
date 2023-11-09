487ee4ca8b58a89cdf94237277aa12ff7bd3df6a
AaronGorbaty <79929603+AaronGorbaty@users.noreply.github.com>
2023-11-09 14:17:24 -0500

    Update environment_set_up.sh
    updated environment_set_up.sh as of 11/09

--- scripts/environment_set_up.sh
+++ scripts/environment_set_up.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Just in case stop all forever running processes
+# sudo forever stopall
+
 # Delete all existing containers
 containers_to_destroy=$(sudo lxc-ls --fancy | tail -n +2 | awk '{print $1}')
 for container in $containers_to_destroy; do
@@ -11,14 +14,14 @@ sh /home/student/scripts/firewall_set_up.sh
 
 # Call the 5 different instances of recycler for the 5 different IPs
 # Note sleep 5s is used to allow set up to work properly, or else too many things running at the same time, causing errors
-/home/student/scripts/recycler.sh "name1_doesnt_exist" "128.8.238.196" "1000" "1234" &
-sleep 5s
-/home/student/scripts/recycler.sh "name2_doesnt_exist" "128.8.238.28" "2000" "1234" &
-sleep 5s
-/home/student/scripts/recycler.sh "name3_doesnt_exist" "128.8.238.46" "3000" "1234" &
-sleep 5s
-/home/student/scripts/recycler.sh "name4_doesnt_exist" "128.8.238.177" "4000" "1234" &
-sleep 5s
-/home/student/scripts/recycler.sh "name5_doesnt_exist" "128.8.238.103" "5000" "1234" &
+/home/student/scripts/recycler.sh "name1_doesnt_exist" "128.8.238.196" "1000" &
+sleep 15s
+/home/student/scripts/recycler.sh "name2_doesnt_exist" "128.8.238.28" "2000" &
+sleep 15s
+/home/student/scripts/recycler.sh "name3_doesnt_exist" "128.8.238.46" "3000" &
+sleep 15s
+/home/student/scripts/recycler.sh "name4_doesnt_exist" "128.8.238.177" "4000" &
+sleep 15s
+/home/student/scripts/recycler.sh "name5_doesnt_exist" "128.8.238.103" "5000" &
 
 echo "Set up of host environment complete."
