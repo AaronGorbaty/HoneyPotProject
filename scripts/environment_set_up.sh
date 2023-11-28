27a484c0ae60d8ff8245d02e4079a4a32aeece7d
AaronGorbaty <79929603+AaronGorbaty@users.noreply.github.com>
2023-11-28 14:50:03 -0500

    Updated environment_set_up.sh

--- scripts/environment_set_up.sh
+++ scripts/environment_set_up.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Just in case stop all forever running processes
-# sudo forever stopall
-
-# Delete all existing containers
+# Delete all existing containers, except for the ones that have snapshots
+# We had a particular naming convention for those specific containers
 containers_to_destroy=$(sudo lxc-ls --fancy | tail -n +2 | awk '{print $1}')
 for container in $containers_to_destroy; do
   if ! echo $container | grep -q "Honeypot"; then
@@ -15,15 +13,14 @@ done
 sh /home/student/scripts/firewall_set_up.sh
 
 # Call the 5 different instances of recycler for the 5 different IPs
-# Note sleep 5s is used to allow set up to work properly, or else too many things running at the same time, causing errors
+# Note: sleep 30s is used to allow set up to work properly, or else too many things running at the same time, causing errors
+# The first argument will be set properly in the recyler.sh script, other arguments are the proper values that we will be using
 /home/student/scripts/recycler.sh "name1_doesnt_exist" "128.8.238.196" "1000" &
-sleep 15s
+sleep 30s
 /home/student/scripts/recycler.sh "name2_doesnt_exist" "128.8.238.28" "2000" &
-sleep 15s
+sleep 30s
 /home/student/scripts/recycler.sh "name3_doesnt_exist" "128.8.238.46" "3000" &
-sleep 15s
+sleep 30s
 /home/student/scripts/recycler.sh "name4_doesnt_exist" "128.8.238.177" "4000" &
-sleep 15s
+sleep 30s
 /home/student/scripts/recycler.sh "name5_doesnt_exist" "128.8.238.103" "5000" &
-
-echo "Set up of host environment complete."
