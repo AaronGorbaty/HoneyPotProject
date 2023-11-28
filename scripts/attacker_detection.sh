e23739f6d81925e0fa411523fbb1bc8c6b2bd2b0
AaronGorbaty <79929603+AaronGorbaty@users.noreply.github.com>
2023-11-28 14:49:06 -0500

    Updated attacker_detection.sh

--- scripts/attacker_detection.sh
+++ scripts/attacker_detection.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Variables to be used, were passed in by recycler.sh
 LOG_FILE="$1"
 CONTAINER_NAME=$2
 EXTERNAL_IP=$3
@@ -7,40 +8,41 @@ MITM_PORT=$4
 RECYCLER_SCRIPT="/home/student/scripts/recycler.sh"
 
 
-# Grabbing the attackers IP so that we can block all other IPs
-# ATTACKER_IP=$(grep "Attacker connected:" "$LOG_FILE"| sed -n -E 's/.* ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) .*/\1/p' | head -n 1)
-
-# While loop that does nothing (represented by ':' ) and proceeds with the rest of the script once an attacker is inside the container
+# While loop that waits for an attacker to connect and login to the container
+# sleep command is used so while loop won't run so quickly and cause system issues
 while ! grep -q "Compromising the honeypot" "$LOG_FILE";
 do
   sleep .5
 done
 
-# Grabbing the attackers IP so that we can block all other IPs
-ATTACKER_IP=$(grep "Threshold" "$LOG_FILE" | sed -n -E 's/.* ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) .*/\1/p' | head -n 1)
+# Grabbing the attackers IP so that we can block all other IPs later (described below)
+ATTACKER_IP=$(grep "Threshold" "$LOG_FILE" | sed -n -E 's/.*Attacker: ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+),.*/\1/p')
 
 # Apply appropriate firewall rules to only allow the attacker currently attacking, once the attacker gets into the container
-# Limit only one session for the root user for the container, prevents two attackers from logging in at the same time
+# Prevents two attackers from logging in at the same time
 sudo lxc-attach -n $CONTAINER_NAME -- sudo ufw delete allow 'OpenSSH'
 sudo lxc-attach -n $CONTAINER_NAME -- sudo ufw allow from $ATTACKER_IP to any port 22 proto tcp
 sudo lxc-attach -n $CONTAINER_NAME -- sudo ufw deny 22/tcp
 
-
-# Using Bash built in variable SECONDS, the while loop will run until 5 minutes is up
-# Or if the grep if statement returns true, meaning the attacker exited the container
+# Using Bash built in variable
 SECONDS=0
 # Keep track of number of commands run, in order to track idle time
 NUM_COMMANDS=0
+# Using Bash built in variable SECONDS, the while loop will run until 10 minutes is up
+# Or if an attacker has been idle for more than 30 seconds, which means no commands run
 while [[ $SECONDS -lt 600 ]];
 do
   # Sleep for 30 seconds, which will be the idle time
   sleep 30
+  # Count the number of commands run
   CURR_COUNT=$(grep -E "reader|Noninteractive" "$LOG_FILE" | wc -l)
+  # If the number of commands run has changed...
   if [[ $CURR_COUNT -ne $NUM_COMMANDS ]]; then
     # Update count of number of commands run so far
     NUM_COMMANDS=$CURR_COUNT
   else
     # This will exit out of the while loop, if the attacker has been idled for 30 seconds
+    # number of commands is the same
     break
   fi
 done
